7397151bef6d3a5b47886a454b7bb94e1fa1e228
Ahmed <63455817+hawaryo@users.noreply.github.com>
2025-02-28 22:15:57 +0200

    Enhance: Created separate Category component for better organization

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,6 @@
+import CategorySection from "../../component/CategorySection";
 import "./page.css";
-import { createClient } from "../../../utils/supabase/server";
-export default async function Instruments() {
-  const supabase = await createClient();
-  const { data: category } = await supabase.from("category").select();
-
+export default function Instruments() {
   return (
     <main>
       <div className="home-page-intro">
@@ -13,32 +10,7 @@ export default async function Instruments() {
           الشمسية والبطاريات والمكونات الإليكترونية وأجهزه القياس
         </p>
       </div>
-      <section className="category-section">
-        <h2>تفقد الأقسام</h2>
-        <div className="category-grid">
-          {category?.map((a) => (
-            <a key={a.id} href={`/${a.name}`}>
-              <div className="image-container">
-                <img src={a.image_url} alt={a.alt} width={172} height={180} />
-              </div>
-              <div className="category-name">
-                <h3>{a.name}</h3>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="16"
-                  fill="none"
-                >
-                  <path
-                    d="M6.885 15.09L.537 8.71a1 1 0 0 1 .004-1.414L6.921.948a1 1 0 1 1 1.411 1.418L3.663 7.011l19.365.05-.005 2-19.365-.05 4.645 4.669a1 1 0 0 1-1.418 1.41z"
-                    fill="#000"
-                  />
-                </svg>
-              </div>
-            </a>
-          ))}
-        </div>
-      </section>
+      <CategorySection />
     </main>
   );
 }
---
+++ src/component/CategorySection.tsx
@@ -0,0 +1,37 @@
+export const runtime = "edge";
+import "./CategorySection.css";
+import { createClient } from "../../utils/supabase/server";
+
+export default async function Instruments() {
+  const supabase = await createClient();
+  const { data: category } = await supabase.from("category").select();
+
+  return (
+    <section className="category-section">
+      <h2>تفقد الأقسام</h2>
+      <div className="category-grid">
+        {category?.map((a) => (
+          <a key={a.id} href={`/${a.name}`}>
+            <div className="image-container">
+              <img src={a.image_url} alt={a.alt} width={172} height={180} />
+            </div>
+            <div className="category-name">
+              <h3>{a.name}</h3>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="16"
+                fill="none"
+              >
+                <path
+                  d="M6.885 15.09L.537 8.71a1 1 0 0 1 .004-1.414L6.921.948a1 1 0 1 1 1.411 1.418L3.663 7.011l19.365.05-.005 2-19.365-.05 4.645 4.669a1 1 0 0 1-1.418 1.41z"
+                  fill="#000"
+                />
+              </svg>
+            </div>
+          </a>
+        ))}
+      </div>
+    </section>
+  );
+}
