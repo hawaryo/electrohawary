99407fa6814dde320e8610f179d6f963ebda4de1
Ahmed <63455817+hawaryo@users.noreply.github.com>
2025-03-07 16:11:55 +0200

    feat: implement data fetching on category page to retrieve products data based on the selected category

--- src/app/(home)/category/[name]/page.tsx
+++ src/app/(home)/category/[name]/page.tsx
@@ -1,7 +1,6 @@
 import "./page.css";
 import {createClient} from "../../../../../utils/supabase/client";
 
-
 type props = {
   params: {
     name: string;
@@ -10,15 +9,23 @@ type props = {
 export default async function CategoryProducts({params}: props) {
   const param = await params;
   const supabase = await createClient();
+  //get category ID
+  const {data: categoryId} = await supabase
+    .from("category")
+    .select(`id`)
+    .eq("name", decodeURIComponent(param.name))
+    .single();
+  //get products from the specified category ID
   const {data: product} = await supabase
     .from("product")
-    .select("*, category(name)")
-    .eq("category.name", `${param.name}`);
-  
+    .select(`title, product_image(url, alt)`)
+    .eq("category_id", categoryId?.id); //param.name
+
   return (
     <div>
-      <div lang="ar">{decodeURIComponent(param.name)}</div>
+      <div>{`منتجات قسم ال${decodeURIComponent(param.name)}`}</div>
       <pre>{JSON.stringify(product, null, 2)}</pre>
+     
     </div>
   );
 }
