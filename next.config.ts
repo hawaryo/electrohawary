f7a8c2b6fb1f384e05b7a79deb9df7ff983e7f36
Ahmed <63455817+hawaryo@users.noreply.github.com>
2025-03-16 21:53:40 +0200

    Update: disable use cache  Cloudflare hosting

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    useCache: true,
+   /*  useCache: true, */
   },
   images: {
     remotePatterns: [
