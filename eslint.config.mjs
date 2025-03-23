dc8ba14ff8183bb30aa66939a336768f141af937
Ahmed <63455817+hawaryo@users.noreply.github.com>
2025-03-23 16:23:45 +0200

    feat(eslint): add support for React compiler in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import {dirname} from "path";
 import {fileURLToPath} from "url";
 import {FlatCompat} from "@eslint/eslintrc";
 import sonarjs from "eslint-plugin-sonarjs";
-
+import reactCompiler from "eslint-plugin-react-compiler";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -12,6 +12,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends(
+    "next",
     "next/core-web-vitals",
     "next/typescript",
     "plugin:jsx-a11y/strict"
@@ -19,11 +20,13 @@ const eslintConfig = [
   {
     plugins: {
       sonarjs: sonarjs,
+      "react-compiler": reactCompiler,
     },
-    rules: sonarjs.configs.recommended.rules,
   },
+
   {
     rules: {
+      ...sonarjs.configs.recommended.rules,
       quotes: ["error", "double"],
       "prefer-const": "error",
       "no-console": "warn",
@@ -35,6 +38,9 @@ const eslintConfig = [
       "consistent-return": "error",
       "no-var": "error",
       "prefer-arrow-callback": "error",
+      "@next/next/no-img-element": "off",
+      "no-console": "warn",
+      "react-compiler/react-compiler": "error",
     },
   },
 ];
