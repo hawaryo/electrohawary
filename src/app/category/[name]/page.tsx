fc41d8c7d46b17f0fcb8b30f5bf14eabe2a42901
Ahmed <63455817+hawaryo@users.noreply.github.com>
2025-04-10 04:40:00 +0200

    feat: display product price conditionally for VIP users

--- src/app/category/[name]/page.tsx
+++ src/app/category/[name]/page.tsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import {createClient} from "../../../utils/supabase/client";
+import {auth} from "../../../utils/auth/auth";
 import Link from "next/link";
 import {Suspense} from "react";
 //types
@@ -11,13 +12,16 @@ type props = {
 
 export default async function CategoryProducts({params}: props) {
   const param = await params;
-  const supabase = createClient();
   const CategoryName = decodeURIComponent(param.name);
+  const supabase = createClient();
+  const session = await auth();
 
   //get products from the specified category
   const {data: products} = await supabase
     .from("product")
-    .select("id, title, product_image!inner(url, alt), category!inner(name), price")
+    .select(
+      "id, title, product_image!inner(url, alt), category!inner(name), price"
+    )
     .eq("category.name", CategoryName);
 
   return (
@@ -37,7 +41,9 @@ export default async function CategoryProducts({params}: props) {
                   height={300}
                 />
                 <h2>{n.title}</h2>
-                <p className={styles["price"]}>{n.price} جنية</p>
+                {session?.user.is_vip ? (
+                  <p className={styles["price"]}>{n.price} جنية</p>
+                ) : null}
               </Link>
             ))}
           </div>
