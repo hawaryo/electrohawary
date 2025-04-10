384d275e751c21e860d0980e272a85c1626f024c
Ahmed <63455817+hawaryo@users.noreply.github.com>
2025-04-10 22:39:38 +0200

    fix: correct import path for UserAccount component in account page

--- src/app/(auth)/account/page.tsx
+++ src/app/(auth)/account/page.tsx
@@ -1,12 +1,11 @@
 import styles from "./page.module.css";
 import SignInForm from "@/components/SignInForm";
-import UserAccount from "@/components/SignInForm";
+import UserAccount from "@/components/UserAccount";
 import {auth} from "../../../utils/auth/auth";
 
-//if user seeins >>>> account  elase >>>>> login
 export default async function Account() {
   const session = await auth();
-
+  
   return (
     <main className={styles["main"]}>
       {session ? <UserAccount /> : <SignInForm />}
