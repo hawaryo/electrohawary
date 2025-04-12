18890f21311f47ed993d0c334fc01c43b6bf0e84
Ahmed <63455817+hawaryo@users.noreply.github.com>
2025-04-12 18:32:14 +0200

    chore: remove unnecessary comment in route.ts import statement

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,2 @@
-import {handlers} from "../../../../utils/auth/auth"; // Referring to the auth.ts we just created
+import {handlers} from "../../../../utils/auth/auth";
 export const {GET, POST} = handlers;
